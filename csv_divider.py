4c7c534c77cbb7d5b427747a75dfebf69c2f50d5
zackcglee <leechanggong@gmail.com>
2023-01-25 19:45:16 +0900

    test10

--- csv_divider.py
+++ csv_divider.py
@@ -2,29 +2,33 @@
 import sys
 
 def main():
-    if sys.argv[1] == '-t':
-        with open(sys.argv[2],'r') as csv:
+    if sys.argv[2] == '-t':
+        with open(sys.argv[3],'r') as csv:
             print(len(csv.readlines()))
             sys.exit()
-    # if len(sys.argv) != 4:
-    #     print('Usage: csv_divider.py [option] <input_path> <output_path> <number_of_files>')
-    #     print('option: -d divide csv file into n files')
-    #     print('option: -t test')
-    #     sys.exit(1)
+    if len(sys.argv) != 4:
+        print('Usage: csv_divider.py [option] <input_path> <output_path> <number_of_files>')
+        print('option: -d divide csv file into n files')
+        print('option: -t test')
+        sys.exit(1)
     
     
-    with open(sys.argv[1],'r') as csv:
+    with open(sys.argv[2],'r') as csv:
+        tmp = []
         lines = csv.readlines()
         size = len(lines)
-        divisor = int(len(lines)/int(sys.argv[3]))
+        divisor = int(len(lines)/int(sys.argv[4]))
         j = 0
         k = 1
         for i in range(0, size):
+            tmp.append(lines[i])
             if j > divisor or i == size -1:
                 j = 0
-                with open(sys.argv[2] + '_' + str(k) + '.csv', 'w') as output:
-                    output.writelines(lines[i:i+100])
+                with open(sys.argv[3] + '_' + str(k) + '.csv', 'w') as output:
+                    output.writelines(lines)
+                    tmp = []
                     k += 1
+            
             j += 1
 
 
